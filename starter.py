e17a86dca86716b911f0d235ff6dfdea6325da85
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2025-09-10 22:00:14 +0530

    Add conversation tool and enhance response streaming functionality

--- starter.py
+++ starter.py
@@ -3,6 +3,9 @@ from llama_index.core.agent.workflow import FunctionAgent, BaseWorkflowAgent
 from llama_index.llms.ollama import Ollama
 from enum import Enum
 from pprint import pprint
+# import llama_index.core.workflow.context.Context as context
+import workflows.context as context
+from typing import List, Sequence, Optional
 
 # Enum for model names
 class Model(Enum):
@@ -18,11 +21,16 @@ def multiply(a: float, b: float) -> float:
     """Useful for multiplying two numbers."""
     return a * b
 
+
+def converse(query: str) -> str:
+    """Useful for general conversation."""
+    return query
+
 # AGENT SETUP
 # --------------------------------
 # Create an agent workflow with our calculator tool
 agent = FunctionAgent(
-    tools=[multiply],
+    tools=[multiply, converse],
     llm=Ollama(
         model=Model.llama3.value,
         request_timeout=360.0,
@@ -34,21 +42,31 @@ agent = FunctionAgent(
 
 # HELPERS
 # --------------------------------
-async def stream_response(agent: FunctionAgent, query: str):
+async def stream_response(agent: FunctionAgent, query: str, ctx: Optional[context.Context] = None):
     """Stream the agent's response."""
-    workflow_handler = agent.run(query)
+    # print(f"User: {query}")
+    workflow_handler = agent.run(query, ctx=ctx)
+    print("Agent:", end=" ", flush=True)
     async for chunk in workflow_handler.stream_events():
         # Filter for LLM text generation events
         if hasattr(chunk, 'delta') and chunk.delta:
             print(chunk.delta, end="", flush=True)
         elif hasattr(chunk, 'text') and chunk.text:
             print(chunk.text, end="", flush=True)
-    print()  # For a newline after streaming
-    
-async def main():
+    print(f"\n{'-'*20}", flush=True)  # For a newline after streaming
+
+async def start_agent():
     print("Starting agent...")
-    # Run the agent
-    await stream_response(agent, "What is 1234 * 4567?")
+    await stream_response(agent, "Hello, what can you do?")
+
+async def main():
+    await start_agent()
+
+    while user_input := input("User: "):
+        await stream_response(agent, user_input)
+
+    # # Run the agent
+    # await stream_response(agent, "What is 1234 * 4567?")
 
 
 # Run the agent
